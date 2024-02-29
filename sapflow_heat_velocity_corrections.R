d6f507cec5ea8280b148093959564395a2e86ae5
Thomas Ohnemus <thomas.ohnemus@ufz.de>
2024-02-29 14:29:56 +0100

    Added weather data to sapflow script

--- sapflow_heat_velocity_corrections.R
+++ sapflow_heat_velocity_corrections.R
@@ -8,19 +8,24 @@ library(data.table)
 library(tidyverse)
 library(suncalc)
 
+setwd("C:/Docs/MIRO/sapflow_CHP/")
 # Read measured variables
-parameters        <- read.csv2("C:/Docs/MIRO/Baumparameter.csv")
+parameters        <- read.csv2("Baumparameter.csv")
 parameters$sheet  <- paste0(parameters$Tree, ".csv")
 p                 <- parameters[parameters$Tree == unique(parameters$Tree)[1],]
 
-# Get Data
-dat               <- read.csv(paste0("C:/Docs/MIRO/Saftfluss/data/",p$sheet))
-dat$Time          <- as.POSIXct(dat$Time, "%Y-%m-%d %H:%M:%S")
+# Get Sapflow data
+dat               <- read.csv(paste0("data_raw/",p$sheet))
+dat$Time          <- as.POSIXct(dat$Time, "%Y-%m-%d %H:%M:%S", tz = "UTC")
+
+# Get weather data
+weather           <- read.csv("data_1st_lvl/LA_weather.csv")
+weather$UTC       <- as.POSIXct(weather$UTC, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
 
 # Background Calculations
-source("C:/Docs/MIRO/Saftfluss/Sapflow_Background.R")
+source("sapflow_chp/Sapflow_functions.R")
 
-    # To Do: Find Diagnostics to clean outliers with in Sapflow_Background.R
+    # To Do: Find Diagnostics to identify flaws in data
 dat_l <- pivot_longer(dat, cols = 2:22, names_to = "variable", values_to = "value")
 dat_l$value[dat_l$value > 100] <- NA
 ggplot(dat_l[dat_l$variable %like% "Tds" | dat_l$variable %like% "SWC",], 
@@ -28,12 +33,16 @@ ggplot(dat_l[dat_l$variable %like% "Tds" | dat_l$variable %like% "SWC",],
   theme_bw() + facet_wrap(~variable, scales = "free", ncol = 1) +
   geom_vline(xintercept = as.POSIXct("2024-02-24 01:43:44", tz = "UTC"), color = "steelblue") +
   geom_vline(xintercept = as.POSIXct("2024-02-24 05:43:42", tz = "UTC"), color = "steelblue") +
-  geom_vline(xintercept = as.POSIXct("2024-02-25 22:13:48", tz = "UTC"), color = "steelblue") 
-ggsave("C:/Docs/MIRO/Saftfluss/Frost.png", dpi = 300, width = 10, height = 15, units = "cm")
+  geom_vline(xintercept = as.POSIXct("2024-02-25 22:13:48", tz = "UTC"), color = "steelblue") +
+  geom_vline(xintercept = as.POSIXct("2024-02-25 20:00:00", tz = "UTC"), color = "red") +
+  geom_line(data = weather, aes(x = UTC, y = Temperatur), colour = "coral")
+  
+ggsave("plots/Frost_100_ambT.png", dpi = 300, width = 14, height = 15, units = "cm")
 
-dat_l$Time[dat_l$variable == "SF.01.TdsOuter..degC." & dat_l$value < 0]
-  # SWC > 100 = wrong
-    # Reasons: either distance between probes or density of dry wood wrongly estimated
+  # 1st Correction: Remove data where ambient temperature is below 0
+neg_T <- weather$UTC[weather$Temperatur < 0]
+dat_l$Time[which.min(neg_T[2])]
+
 
     # To Do: Data gap 23rd of February early morning? (SWC)
 
